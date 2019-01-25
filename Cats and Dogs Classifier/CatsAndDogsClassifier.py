d55e64ab2b2cab752f28704776fc1dbc4270e11b
JamesPeralta <jamesperalta35@gmail.com>
2019-01-24 23:30:27 -0700

    Introduce pre-trained covnets to increase accuracy of model

--- Cats and Dogs Classifier/CatsAndDogsClassifier.py
+++ Cats and Dogs Classifier/CatsAndDogsClassifier.py
@@ -5,6 +5,7 @@ from keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 from IPython.display import display
 from PIL import Image
+from keras.applications import VGG16
 
 # Directories on disk where we will store data
 original_dataset_dir = '/Users/jamesperalta/Datasets/Unzipped/train'
@@ -32,6 +33,7 @@ def create_model():
     model.add(layers.Conv2D(128, (3, 3), activation='relu'))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Flatten())
+    model.add(layers.Dropout(0.5))
     model.add(layers.Dense(512, activation='relu'))
     model.add(layers.Dense(1, activation='sigmoid'))
 
@@ -44,46 +46,77 @@ train_dir = '/Users/jamesperalta/Datasets/Partitioned/CatsAndDogs/train'
 validation_dir = '/Users/jamesperalta/Datasets/Partitioned/CatsAndDogs/validation'
 test_dir = '/Users/jamesperalta/Datasets/Partitioned/CatsAndDogs/test'
 
-train_datagen = ImageDataGenerator(rescale=1./255)
+# Add Data Augmentation to your training data
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,)
 validation_datagen = ImageDataGenerator(rescale=1./255)
 test_datagen = ImageDataGenerator(rescale=1./255)
 
-train_generator = train_datagen.flow_from_directory(train_dir, target_size=(150, 150), batch_size=20, class_mode='binary')
-validation_generator = validation_datagen.flow_from_directory(validation_dir, target_size=(150, 150), batch_size=20, class_mode='binary')
-test_generator = test_datagen.flow_from_directory(test_dir, target_size=(150, 150), batch_size=20, class_mode='binary')
+# Create Generators from the directory's
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode='binary')
+
+validation_generator = validation_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode='binary')
+
+test_generator = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode='binary')
+
+
+con_base = VGG16(weights='imagenet',
+                 include_top=False,
+                 input_shape=(150, 150, 3))
+
+print(con_base.summary())
+
 
 # After pre-processing fit your model
-model = create_model()
-history = model.fit_generator(
-      train_generator,
-      steps_per_epoch=100,
-      epochs=30,
-      validation_data=validation_generator,
-      validation_steps=50)
-
-model.save('cats_and_dogs_small_1.h5')
-
-# Plot your data here
-acc = history.history['acc']
-val_acc = history.history['val_acc']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-
-epochs = range(1, len(acc) + 1)
-
-plt.plot(epochs, acc, 'bo', label='Training acc')
-plt.plot(epochs, val_acc, 'b', label='Validation acc')
-plt.title('Training and validation Accuracy')
-plt.legend()
-
-plt.figure()
-
-
-plt.plot(epochs, loss, 'bo', label='Training loss')
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
-plt.title('Training and validation loss')
-plt.legend()
-plt.show()
+# model = create_model()
+# history = model.fit_generator(
+#       train_generator,
+#       steps_per_epoch=100,
+#       epochs=100,
+#       validation_data=validation_generator,
+#       validation_steps=50)
+#
+# model.save('cats_and_dogs_small_1.h5')
+#
+# # Plot your data here
+# acc = history.history['acc']
+# val_acc = history.history['val_acc']
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+#
+# epochs = range(1, len(acc) + 1)
+#
+# plt.plot(epochs, acc, 'bo', label='Training acc')
+# plt.plot(epochs, val_acc, 'b', label='Validation acc')
+# plt.title('Training and validation Accuracy')
+# plt.legend()
+#
+# plt.figure()
+#
+#
+# plt.plot(epochs, loss, 'bo', label='Training loss')
+# plt.plot(epochs, val_loss, 'b', label='Validation loss')
+# plt.title('Training and validation loss')
+# plt.legend()
+# plt.show()
 
 
 
